d0bb334a96bd40678af73d1ceccd3155f8cd17e9
Md. Jahirul Islam Shozib <102477252+Jahirul-Islam-Shozib@users.noreply.github.com>
2022-05-25 17:01:12 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+"use scrict";
+// var inputSize = prompt("Enter Input Size");
+// var strArr = [];
+// var nwStr = " ";
+// var flag = true;
+// for (let i = 0; i < inputSize; i++) {
+//   strArr[i] = prompt("Enter Input");
+//   nwStr += strArr[i];
+//   for (let j = 0; j < nwStr.length; j++) {
+//     var strVal = nwStr[j].charCodeAt();
+//     if (strVal >= 48 && strVal <= 57) {
+//       flag = false;
+//     }
+//   }
+//   if (flag === true) {
+//     console.log("The string is clean");
+//   } else if (flag === false) {
+//     console.log("The string is dirty");
+//   }
+// }
+
+// console.log(strArr);
+
+console.log(2 - "3" + 4);
